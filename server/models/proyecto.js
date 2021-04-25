04be72082b1631248c2d98a23bd78f7b2e3edb84
matias25pinto <matias25pinto@gmail.com>
2021-04-25 15:09:42 -0400

    ADD(Cofngiruacion): Se agrego el modulo de configuracion

--- server/models/proyecto.js
+++ server/models/proyecto.js
@@ -9,10 +9,6 @@ const estados = {
 };
 
 const tarea = {
-  id:{
-    type:Number,
-    require:[true, "El id es obligatorio"]
-  },
   titulo: {
     type: String,
     require: [true, "La tarea es obligatoria"],
@@ -22,15 +18,45 @@ const tarea = {
     required: [true, "El estado es obligatorio"],
     enum: estados,
   },
+  lineaBase: {
+    type: Number,
+    require: [true, "Es obligatorio saber si una tarea esta en línea base"],
+    default: 0,
+  },
+  version: {
+    type: Number,
+    require: [true, "El estado es obligatorio"],
+    default: 1,
+  },
   descripcion: {
     type: String,
     required: [true, "La descripción es obligatoria"],
   },
   id_tarea_padre: {
-    type: Number,
-    require:false,
+    type: String,
+    require: false,
   },
   id_tareas_hijos: [],
+};
+
+const lineaBase = {
+  lineaBase: {
+    type: Number,
+    require: [true, "El número de línea base es obligatorio"],
+  },
+  descripcion: {
+    type: String,
+    require: [true, "La descripción de la línea base es obligatoria"],
+  },
+  abierto: {
+    type: Boolean,
+    require: [
+      true,
+      "Indicar que la línea base esta abierto o no es obligatoria",
+    ],
+    default: true,
+  },
+  tareas: [tarea],
 };
 
 const proyectoSchema = Schema({
@@ -39,16 +65,12 @@ const proyectoSchema = Schema({
     requiere: [true, "El nombre del proyecto es obligatorio"],
     unique: true,
   },
-  gerente: {
-    type: Schema.Types.ObjectId,
-    ref: "Usuario",
-    require: [true, "El gerente de proyecto es obligatorio"],
-  },
   descripcion: {
     type: String,
     require: [true, "La descripción del proyecto es obligatoria"],
   },
   tareas: [tarea],
+  lineasBase: [lineaBase],
 });
 
 proyectoSchema.plugin(uniqueValidator, { message: "{PATH} debe de ser único" });
--- server/routes/proyecto.js
+++ server/routes/proyecto.js
@@ -22,11 +22,10 @@ app.get("/proyectos", (req, res) => {
 app.post("/proyecto", (req, res) => {
   let body = req.body;
   let nombre = body.nombre;
-  let gerente = body.gerente;
   let descripcion = body.descripcion;
   let tareas = [];
 
-  if (!nombre || !gerente || !descripcion) {
+  if (!nombre || !descripcion) {
     return res.status(500).json({
       ok: false,
       err: {
@@ -37,7 +36,6 @@ app.post("/proyecto", (req, res) => {
 
   let proyecto = new Proyecto({
     nombre,
-    gerente,
     descripcion,
     tareas,
   });
@@ -60,9 +58,9 @@ app.put("/proyecto/:id", (req, res) => {
   let body = req.body;
   let update = {
     nombre: body.nombre,
-    gerente: body.gerente,
     descripcion: body.descripcion,
     tareas: body.tareas,
+    lineasBase: body.lineasBase
   };
   Proyecto.findByIdAndUpdate(
     id,
@@ -81,6 +79,23 @@ app.put("/proyecto/:id", (req, res) => {
       });
     }
   );
+});
+
+app.delete("/proyecto/:id", (req, res) => {
+  let id = req.params.id;
+  Proyecto.findByIdAndDelete(id, (err, proyectoBorrado) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      message: "Proyecto eliminado",
+      proyectoBorrado,
+    });
+  });
 });
 
 module.exports = app;
